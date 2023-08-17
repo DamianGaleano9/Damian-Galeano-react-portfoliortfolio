bfca83f857de4097ff7835d5d8b025afab576dd7
Damian Galeano <dj.damian.b@hotmail.com>
2023-08-17 20:55:54 +0200

    Introduction to React Forms

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react"
 
 export default class Login extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
+            email: "",
+            password: ""
+
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        console.log("handle change", event);
     }
 
     render() {
@@ -13,14 +29,29 @@ export default class Login extends Component {
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
-                <form>
-                    <input type="text" />
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your Email"
+                        value={this.state.email}
+                        onChange={this.handleChange} />
 
                 </form>
                 <form>
-                    <input type="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Your Password"
+                        value={this.state.password}
+                        onChange={this.handleChange} />
+
 
                 </form>
+
+                <div>
+                    <button type="subtmit">Login Mr Damian</button>
+                </div>
             </div>
         )
     }
