21ce5cdcfae7a33b7b63b1d8d099c4fdb4a9b6bf
Damian Galeano <dj.damian.b@hotmail.com>
2023-08-24 19:12:52 +0200

    Portfolio Manager Grid Layout Implementation

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 
 
 export default class PortfolioManager extends Component {
-    render(){
+    render() {
         return (
             <div className="portfolio-manager-wrapper">
 
                 <div className="left-column">
-<h1>PortfolioManager</h1>
-                     
+                    <h1>PortfolioManager</h1>
                 </div>
 
 
                 <div className="right-column">
-                    
+                    <h1>PortfolioManager</h1>
                 </div>
-                <h1>PortfolioManager</h1>
             </div>
         )
     }
-   
+
 }
