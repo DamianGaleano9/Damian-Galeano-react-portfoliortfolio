90e163efcf788a2eeb37367d9348a7354e85270d
Damian Galeano <dj.damian.b@hotmail.com>
2023-08-24 19:39:15 +0200

    Populating the Portfolio Manager State with API Call Data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 
 export default class PortfolioManager extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            portfolioItem: []
+        }
+    }
+
+    getPortfolioItems(){
+        axios.get("https://jordan.devcamp.space/portfolio/portfolio_items", {withCredentials: true})
+        .then(response => {
+            this.setState({
+                portfolioItem: [... response.data.portfolioItem]
+            })
+        }).catch(error => {
+            console.log("Error in get portfolio", error);
+        });
+    }
+
+
+
+
+    componentDidMount(){
+        this.getPortfolioItems();
+    }
     render() {
         return (
             <div className="portfolio-manager-wrapper">
