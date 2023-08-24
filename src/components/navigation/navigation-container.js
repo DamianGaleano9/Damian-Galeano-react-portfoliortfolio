5db4502509a2ef21c7af2940d3e7578a59faac5a
Damian Galeano <dj.damian.b@hotmail.com>
2023-08-24 16:31:32 +0200

    Finalizing the Logout Functionality with a Higher Order Component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,7 +1,25 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 
 const NavigationComponent = (props) => {
+    const handleSingOut = () => {
+        axios.delete("https://api.devcamp.space/logout", { withCredentials: true })
+            .then(response => {
+                if (response.status === 200) {
+                    props.history.push("/");
+                    props.handleSuccessfulLogout();
+                }
+
+                return response.data
+            }).catch(error => {
+                console.log("Error Singing out", error)
+            })
+    }
+
+
+
     const dynamyc = (route, linkText) => {
         return (
             <div className="nav-link-wrapper">
@@ -41,17 +59,18 @@ const NavigationComponent = (props) => {
                 </div>
 
 
-                {props.loggedInStatus === "LOGGED_IN" ? dynamyc("/blog", "Blog"): null}
+                {props.loggedInStatus === "LOGGED_IN" ? dynamyc("/blog", "Blog") : null}
 
 
             </div>
             <div className="right-side">
                 Damian Mazo Galeano
+                {props.loggedInStatus === 'LOGGED_IN' ? (<a onClick={handleSingOut}>Sing Out</a>) : null}
+
             </div>
         </div>
     );
+};
 
-}
 
-
-export default NavigationComponent;
+export default withRouter(NavigationComponent);
