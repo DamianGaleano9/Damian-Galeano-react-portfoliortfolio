32d84438f8c0849ce2783ed46e6a19c494cfc135
Damian Galeano <dj.damian.b@hotmail.com>
2023-08-25 18:30:03 +0200

    Creating the Portfolio Form and Calling it from the Parent Component

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,44 +2,66 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import PortfolioSideBarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
-    constructor(){
-        super()
+    constructor() {
+        super();
 
-        this.state={
+
+
+        this.state = {
             portfolioItems: []
         }
+
+
+        this.handleSuccessfulFormSubmision = this.handleSuccessfulFormSubmision.bind(this);
+        this.handleFormSubmisionError = this.handleFormSubmisionError.bind(this);
     }
 
-    getPortfolioItems(){
-        axios.get("https://jordan.devcamp.space/portfolio/portfolio_items", {withCredentials: true})
-        .then(response => {
-            this.setState({
-                portfolioItems: [... response.data.portfolio_items]
-            })
-        }).catch(error => {
-            console.log("Error in get portfolio", error);
-        });
+    handleSuccessfulFormSubmision(portfolioItem) {
+        // TODO
+        // update the portfolioItems state
+        // and add the portfolioItem to the list
     }
 
 
+    handleFormSubmisionError(error) {
+        console.log("handleFormSubmisionError", error)
+    }
 
 
-    componentDidMount(){
+    getPortfolioItems() {
+        axios.get("https://jordan.devcamp.space/portfolio/portfolio_items", { withCredentials: true })
+            .then(response => {
+                this.setState({
+                    portfolioItems: [...response.data.portfolio_items]
+                });
+            }).catch(error => {
+                console.log("Error in get portfolio", error);
+            });
+    }
+
+    componentDidMount() {
         this.getPortfolioItems();
     }
+
     render() {
         return (
             <div className="portfolio-manager-wrapper">
 
                 <div className="left-column">
-                    <h1>Portfolio Form</h1>
+
+                    <PortfolioForm
+                        handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                        handleFormSubmissionError={this.handleFormSubmissionError}
+
+                    />
                 </div>
 
 
                 <div className="right-column">
-                <PortfolioSideBarList data={this.state.portfolioItems} />
+                    <PortfolioSideBarList data={this.state.portfolioItems} />
                 </div>
             </div>
         )
