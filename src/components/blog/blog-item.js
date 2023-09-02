f8e1c7dd6d62cceb8281be25dbc3479554ae7837
Damian Galeano <dj.damian.b@hotmail.com>
2023-09-02 22:00:15 +0200

    Creating a Dedicated Blog Item Component

---
+++ src/components/blog/blog-item.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+const BlogItem = props => {
+    const {
+        id,
+        blog_status,
+        content,
+        title,
+        featured_image_url
+
+    } = props.blogItem;
+
+    return (
+        <div>
+            <h1>{title}</h1>
+
+
+            <div>
+                <h1>{content}</h1>
+            </div>
+            
+        </div>
+
+    )
+}
+
+
+
+
+export default BlogItem;
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,23 +1,43 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
     constructor() {
         super();
 
-
+    this.state = {
+        blogItems: []
+    }
+    
+    this.getBlogItems = this.getBlogItems.bind(this);
     }
 
+
+    getBlogItems() {
+        axios.get("https://damiangaleano.devcamp.space/portfolio/portfolio_blogs", {withCredentials: true}).
+        then(response => {
+            this.setState({
+                blogItems: response.data.portfolio_blogs
+            })
+        }
+        ).catch(error => {
+            console.log('Response', error);
+        })
+    }
+
+    componentWillMount(){
+        this.getBlogItems();
+    }
+
+
     render() {
-        return (
-            <div>
-                <h2>Damian</h2>
-                <div>
-                    <Link to="/about-me">Read more About Myself</Link>
-                </div>
-            </div>
-        )
+        const blogRecords = this.state.blogItems.map(blogItem => {
+            return <BlogItem key={blogItem.id} blogItem={blogItem}/>
+        });
+
+        return <div>{blogRecords}</div>
     }
 
 }
