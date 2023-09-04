5f1d6d2be519537e360a482018b01019b798af81
Damian Galeano <dj.damian.b@hotmail.com>
2023-09-04 16:04:56 +0200

    How to Close the React Modal

--- src/components/modals/blog-modal.js
+++ src/components/modals/blog-modal.js
@@ -1,28 +1,19 @@
 import React, { Component } from "react";
-import Reactmodal from "react-modal";
-
-
+import ReactModal from "react-modal";
 
 export default class BlogModal extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
-        this.state = {
+  render() {
+    return (
+      <ReactModal onRequestClose={()=>{
+        this.props.handleModalClose();
 
-        }
-
-        
-    }
-
-
-    render() {
-        return (
-            <div>
-                <Reactmodal isOpen={this.state.modalIsOpen}>
-                    <h1>I'm an a Modal</h1>
-
-                </Reactmodal>
-            </div>
-        )
-    }
+      }} isOpen={this.props.modalIsOpen}>
+        <h1>I'm in a modal!</h1>
+      </ReactModal>
+    );
+  }
 }
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,99 +1,118 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BlogModal from "../modals/blog-modal";
 
-
 class Blog extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            blogItems: [],
-            totalCount: 0,
-            currentPage: 0,
-            isLoading: true,
-            ModalIsOpen: false
+    this.state = {
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
+      isLoading: true,
+      blogModalIsOpen: false
+    };
 
-        }
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+  }
 
-        this.getBlogItems = this.getBlogItems.bind(this);
-        this.onScroll = this.onScroll.bind(this);
-        window.addEventListener("scroll", this.onScroll, false);
-        this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
-    }   
 
-
-    handleNewBlogClick() {
+    handleModalClose(){
         this.setState({
-            ModalIsOpen: true
+            blogModalIsOpen: false
         })
     }
 
 
-    onScroll() {
-            if(this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
-                 return;  
-            }
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true
+    });
+  }
 
-            if (
-                window.innerHeight + document.documentElement.scrollTop ===
-                document.documentElement.offsetHeight
-            ) {
-                this.getBlogItems();
-            }
+  onScroll() {
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
     }
 
-    getBlogItems() {
-        this.setState({ currentPage: this.state.currentPage + 1 });
-        axios.get(`https://damiangaleano.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, { withCredentials: true }).
-            then(response => {
-                this.setState({
-                    blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
-                    totalCount: response.data.meta.total_records,
-                    isLoading: false
-                })
-            }
-            ).catch(error => {
-                console.log('getBlogItems error', error);
-            })
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems();
     }
+  }
 
-    componentWillMount() {
-        this.getBlogItems();
-    }
+  getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
 
-
-    componentWillUnmount(){
-        this.removeEventListener('scroll', this.onScroll, false);
-    }
-
-    render() {
-        const blogRecords = this.state.blogItems.map(blogItem => {
-            return <BlogItem key={blogItem.id} blogItem={blogItem} />
+    axios
+      .get(
+        `https://damiangaleano.devcamp.space/portfolio/portfolio_blogs?page=${this
+          .state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
+      .then(response => {
+        console.log("gettting", response.data);
+        this.setState({
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+          totalCount: response.data.meta.total_records,
+          isLoading: false
         });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
 
-        return (
-            <div className="blog-container">
-                <BlogModal ModalIsOpen={this.state.ModalIsOpen}/>    
+  componentWillMount() {
+    this.getBlogItems();
+  }
 
-                <div className="new-blog-link"> 
-                    <a onClick={this.handleNewBlogClick}>Modal</a> 
-                </div>
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
 
-                <div className="content-container">{blogRecords}</div>
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
 
-                {this.state.isLoading ? (<div className="content-loader">
-                    <FontAwesomeIcon icon="fa-solid fa-spinner" spin />
-                </div>): null}
-                
-            </div>
-        );
-    }
+    return (
+      <div className="blog-container">
+        <BlogModal
+        handleModalClose={this.handleModalClose}
+        modalIsOpen={this.state.blogModalIsOpen} />
+        
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>Open Modal!</a>
+        </div>
 
+        <div className="content-container">{blogRecords}</div>
+
+        {this.state.isLoading ? (
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-
-export default Blog; 
+export default Blog;
